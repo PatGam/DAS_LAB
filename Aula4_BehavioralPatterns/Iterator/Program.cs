94e56f8444e8451c00b1ffc0e8d465f02995ab5c
patriciagameiroit@gmail.com <patriciagameiroit@gmail.com>
2023-01-28 16:48:13 +0000

    Implemented iterator

--- Aula4_BehavioralPatterns/Iterator/Program.cs
+++ Aula4_BehavioralPatterns/Iterator/Program.cs
@@ -1,14 +1,27 @@
-﻿using System.Collections.Generic;
-using System;
-
-namespace Iterator
+﻿
+public static class Program
 {
-    internal class Program
+    public static double CheckAbove(IEnumerable<int> data, int threshold)
     {
-        static void Main(string[] args)
+        int countAbove = 0;
+        int totalCount = 0;
+        foreach (int i in data)
         {
-            int[] a = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
+            if (i > threshold)
+            {
+                countAbove++;
+            }
+            totalCount++;
         }
+
+        return (double)countAbove / totalCount * 100;
+    }
+    public static void Main()
+    {
+        int[] a = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        List<int> lst = a.ToList();
+        HashSet<int> set = new HashSet<int>(lst);
+        Console.WriteLine("% in list is " + CheckAbove(lst, 5));
+        Console.WriteLine("% in set is " + CheckAbove(set, 5));
     }
 }
